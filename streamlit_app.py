545cc837715a9810db484d4b80c212cebb49f712
MohammedMajid <91215902+mdmajduxli@users.noreply.github.com>
2022-12-11 23:16:37 +0530

    Update streamlit_app.py
    code changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ except URLError as e:
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-streamlit.header("View Our Fruit List - Add Your Favourites!")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
